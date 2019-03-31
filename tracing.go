f8a60076e2de5fbf8549b759618ddfafe6965f82
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2019-03-30 17:53:57 -0700

    Update tracing.go

--- tracing.go
+++ tracing.go
@@ -1,18 +1,28 @@
-package tracer
+package tracing
 
 import (
-    "io"
-    
-    "github.com/uber/jaeger-client-go/config"
-    jprom "github.com/uber/jaeger-lib/metrics/prometheus"
+	"fmt"
+	"io"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	config "github.com/uber/jaeger-client-go/config"
 )
 
-func NewTracer() (opentracing.Tracer, io.Closer, error) {
-    // load config from environment variables
-    cfg, _ := jaegercfg.FromEnv()
-
-    // create tracer from config
-    return cfg.NewTracer(
-        config.Metrics(jprom.New()),
-    )
+// Init returns an instance of Jaeger Tracer that samples 100% of traces and logs all spans to stdout.
+func Init(service string) (opentracing.Tracer, io.Closer) {
+	cfg := &config.Configuration{
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	tracer, closer, err := cfg.New(service, config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+	}
+	return tracer, closer
 }
